250f54b8a38ac395b9714ee1a29809d5504ada0d
Dulanga Wimalagunasekara <153906345+DulangaMW@users.noreply.github.com>
2024-01-06 20:01:51 +0530

    Add files via upload
    Initial Commit

---
+++ src/main/java/com/innobothealth/accessmanagementsystem/AccessManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.innobothealth.accessmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innobothealth/accessmanagementsystem/AccessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innobothealth.accessmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
