ada048dec835e5d1808f94561e3380a98c561d2b
DulangaMW <dulanga.wimalagunasekara@arimaclanka.com>
2024-04-07 10:49:47 +0530

    Add info log for automated deployment debugging purposes

--- src/main/java/com/innobothealth/accessmanagementsystem/controller/UserController.java
+++ src/main/java/com/innobothealth/accessmanagementsystem/controller/UserController.java
@@ -3,6 +3,7 @@ package com.innobothealth.accessmanagementsystem.controller;
 import com.innobothealth.accessmanagementsystem.document.User;
 import com.innobothealth.accessmanagementsystem.dto.UserDTO;
 import com.innobothealth.accessmanagementsystem.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -22,6 +24,7 @@ public class UserController {
 
     @PostMapping("register")
     public ResponseEntity<User> registerUser(@RequestBody @Validated UserDTO user) {
+        log.info("User Registration for [{}]", user);
         return userService.registerUser(user);
     }
 
