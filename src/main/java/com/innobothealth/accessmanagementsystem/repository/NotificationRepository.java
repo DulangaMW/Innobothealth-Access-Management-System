c43cc9277c430ce2a3b38bad4247ca0fcfaf0d88
DulangaMW <dulanga.wimalagunasekara@arimaclanka.com>
2024-04-23 14:09:59 +0530

    Add Notification APIs - Part II

--- src/main/java/com/innobothealth/accessmanagementsystem/repository/NotificationRepository.java
+++ src/main/java/com/innobothealth/accessmanagementsystem/repository/NotificationRepository.java
@@ -4,6 +4,11 @@ import com.innobothealth.accessmanagementsystem.document.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+    List<Notification> findAllBySenderId(String receiver);
+    Notification findByIdAndReceiverId(String id, String receiverId);
 }
