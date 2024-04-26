7fda14961329d96c315a48734fda925633e3673f
DulangaMW <dulanga.wimalagunasekara@arimaclanka.com>
2024-04-26 20:28:21 +0530

    Revert "Deletion"
    This reverts commit f32088eb2b3cb1940689e64ae36ad12814ea4f4e.

--- src/main/java/com/innobothealth/accessmanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/innobothealth/accessmanagementsystem/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.GET,"/test/send-sms").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/admin/register").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/admin/getAll").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/user/delete").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/admin/otp/request").permitAll()
                                 .requestMatchers(HttpMethod.DELETE,"/admin/delete/{id}").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/admin/request/token").permitAll()
