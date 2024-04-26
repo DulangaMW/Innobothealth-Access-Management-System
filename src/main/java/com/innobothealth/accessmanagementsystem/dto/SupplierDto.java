38ff40366e1946a15ca21aa2104886c3357d32a3
Arunalu Bamunusinghe <130811052+ArunaluB@users.noreply.github.com>
2024-04-26 20:47:56 +0530

    Update SupplierDto.java

--- src/main/java/com/innobothealth/accessmanagementsystem/dto/SupplierDto.java
+++ src/main/java/com/innobothealth/accessmanagementsystem/dto/SupplierDto.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class SupplierDto {
-    private String companyName;
+ 
     private String supplier_name;
     private String contact_person;
     private String phone;
