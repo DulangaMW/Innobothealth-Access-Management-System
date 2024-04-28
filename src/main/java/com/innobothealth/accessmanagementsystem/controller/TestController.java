922bf15033cace9fda74ac6ffb419e53700c291b
DulangaMW <dulanga.wimalagunasekara@arimaclanka.com>
2024-04-28 13:55:09 +0530

    Hello 4

--- src/main/java/com/innobothealth/accessmanagementsystem/controller/TestController.java
+++ src/main/java/com/innobothealth/accessmanagementsystem/controller/TestController.java
@@ -23,7 +23,7 @@ public class TestController {
 
     @GetMapping("say-hello")
     public String sayHello() {
-        return "Updated Hello -3!!";
+        return "Updated Hello -4!!";
     }
 
     @GetMapping("send-email")
